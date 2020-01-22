69abc59058999cbfef60f5708f337310f5cf897f
Craig Wisniewski <craig.wisniewski@gmail.com>
2020-01-22 20:50:14 +1030

    Added license comments to files

--- src/main/java/net/rptools/maptool/mtresource/MTResourceBundle.java
+++ src/main/java/net/rptools/maptool/mtresource/MTResourceBundle.java
@@ -1,3 +1,17 @@
+/*
+ * This software Copyright by the RPTools.net development team, and
+ * licensed under the Affero GPL Version 3 or, at your option, any later
+ * version.
+ *
+ * MapTool Source Code is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License * along with this source Code.  If not, please visit
+ * <http://www.gnu.org/licenses/> and specifically the Affero license
+ * text at <http://www.gnu.org/licenses/agpl.html>.
+ */
 package net.rptools.maptool.mtresource;
 
 import java.util.Optional;
@@ -5,7 +19,6 @@ import java.util.Optional;
 public interface MTResourceBundle {
 
   String getName();
-
 
   void setName(String bundleName);
 
@@ -20,5 +33,4 @@ public interface MTResourceBundle {
   Optional<MTResource> getResource(String path);
 
   void putResource(String path, MTResource res);
-
 }
