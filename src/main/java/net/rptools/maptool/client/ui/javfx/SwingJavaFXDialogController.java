d4eb72eea30b59d3b087943fe013c2c541d7e083
Craig Wisniewski <craig.wisniewski@gmail.com>
2021-03-27 05:45:40 +1030

    Added classes to display FXML embeded in swing dialog

---
+++ src/main/java/net/rptools/maptool/client/ui/javfx/SwingJavaFXDialogController.java
@@ -0,0 +1,7 @@
+package net.rptools.maptool.client.ui.javfx;
+
+public interface SwingJavaFXDialogController {
+
+  void registerEventHandler(SwingJavaFXDialogEventHandler handler);
+
+}
---
+++ src/main/java/net/rptools/maptool/client/ui/javfx/SwingJavaFXDialogEventHandler.java
@@ -0,0 +1,5 @@
+package net.rptools.maptool.client.ui.javfx;
+
+public interface SwingJavaFXDialogEventHandler {
+  void close();
+}
