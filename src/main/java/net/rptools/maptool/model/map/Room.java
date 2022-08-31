457a3b7f6953c0c876d2be5611aed050bd0270a2
Craig Wisniewski <craig.wisniewski@gmail.com>
2022-08-31 18:36:08 +0930

    bookmarks

---
+++ src/main/java/net/rptools/maptool/model/map/Room.java
@@ -0,0 +1,26 @@
+package net.rptools.maptool.model.map;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public class Room {
+  private final UUID id;
+  private String name;
+  private String mapKey;
+  private String notes;
+  private String gmNotes;
+  private boolean visibleToPlayers;
+  private final List<Point> vertices = new ArrayList<>();
+  private final Set<Encounter> encounters = new HashSet<>();
+  private Bookmark bookmark;
+
+  public Room(String name) {
+    id = UUID.randomUUID();
+    this.name = name;
+  }
+
+}
